f4258cf5abff41afd287f049ea9a4b51fbdf3f52
hivip <p2004hitarth@gmail.com>
2024-12-24 00:20:16 -0500

    print and remove default Ingredients Working for FoodItem

--- src/io/TimHaritonsPOS/Items/FoodItems/FoodItem.java
+++ src/io/TimHaritonsPOS/Items/FoodItems/FoodItem.java
@@ -12,7 +12,7 @@ public abstract class FoodItem implements Item {
     private final int id;
     private final String ItemName;
     private final double price;
-    private final LinkedList<NamePricePair> ingredients;
+    private LinkedList<NamePricePair> ingredients;
     private LinkedList<NamePricePair> modifications = new LinkedList<>();
     private final ArrayList<NamePricePair> modifiers = new ArrayList<>(Arrays.asList(
             new NamePricePair("Cheese", 1.00),
@@ -33,16 +33,6 @@ public abstract class FoodItem implements Item {
         listOfModifiers();
         setModifications();
     }
-    private void setModifications(){
-        Scanner sc = new Scanner(System.in);
-        int modifierNum = 0;
-        while (true) {
-            System.out.println("Enter the number to add Modifiers : ");
-            modifierNum = sc.nextInt();
-            if (modifierNum == 9) break;
-            modifications.add(modifiers.get(modifierNum-1));
-        }
-    }
     public double getPrice() {
         double totalPrice = price;
         for (NamePricePair modifier: modifications) {
@@ -54,6 +44,9 @@ public abstract class FoodItem implements Item {
     public int getId() {
         return id;
     }
+    public LinkedList<NamePricePair> getingredients(){
+        return ingredients;
+    }
 
     private void listOfModifiers(){
         System.out.println("Pick Modifications for " + ItemName + " or else ENTER 9");
@@ -61,6 +54,25 @@ public abstract class FoodItem implements Item {
             System.out.printf("%-" + 30 + "s %s\n", (i+1) +". "+  modifiers.get(i).toString(), (i+2) +". "+ modifiers.get(i+1).toString());
         }
         System.out.println("9. No more Addition!!!");
+        System.out.println("10. To Remove Item!!!");
+    }
+
+    private void setModifications(){
+        Scanner sc = new Scanner(System.in);
+        int modifierNum = 0;
+        while (true) {
+            System.out.println("Enter the number to add Modifiers : ");
+            modifierNum = sc.nextInt();
+            if (modifierNum == 9) break;
+            else if (modifierNum == 10) {
+                printIngredients();
+                System.out.println("Enter num to remove : ");
+                removeIngredients(sc.nextInt());
+                printIngredients();
+                continue;
+            }
+            modifications.add(modifiers.get(modifierNum-1));
+        }
     }
 
     private String printModification(){
@@ -75,6 +87,15 @@ public abstract class FoodItem implements Item {
         }
     }
 
+    private void printIngredients(){
+        for (int i = 0; i< ingredients.size(); i++){
+            System.out.println( (i+1) +". "+  ingredients.get(i).toString() );
+        }
+    }
+
+    public void removeIngredients(int id){
+        ingredients.remove(id-1);
+    }
 
 
     @Override
--- src/io/TimHaritonsPOS/NamePricePair.java
+++ src/io/TimHaritonsPOS/NamePricePair.java
@@ -1,5 +1,7 @@
 package io.TimHaritonsPOS;
 
+import java.util.Objects;
+
 public class NamePricePair {
     private final String name;
     private final double price;
@@ -15,6 +17,19 @@ public class NamePricePair {
 
     public double getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        //NamePricePair that = (NamePricePair) o;
+        return Objects.equals(name, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     @Override
