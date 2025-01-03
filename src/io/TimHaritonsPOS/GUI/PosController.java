b6ac64ac3b814d90322f617d2ebf95273270b0cc
hivip <p2004hitarth@gmail.com>
2025-01-03 17:23:06 -0500

    GUI is ready. But not controller class. Java classes are not connected with JavaFX classes

---
+++ src/io/TimHaritonsPOS/GUI/PosController.java
@@ -0,0 +1,11 @@
+package io.TimHaritonsPOS.GUI;
+
+import javafx.fxml.FXML;
+
+public class PosController {
+
+    @FXML
+    private void handleButtonClick() {
+        System.out.println("Button clicked!");
+    }
+}
---
+++ src/io/TimHaritonsPOS/GUI/posApplication.java
@@ -0,0 +1,36 @@
+package io.TimHaritonsPOS.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class posApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Load the FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("pos.fxml"));
+            Parent root = loader.load();
+
+            // Set the scene
+            Scene scene = new Scene(root);
+
+            // Configure the stage
+            primaryStage.setTitle("POS Application");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
